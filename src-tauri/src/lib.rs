a899e5faea9c62bf9777cab613c52faa4cabdbe5
VacuolePaoo <161350352+VacuolePaoo@users.noreply.github.com>
2025-09-19 12:42:22 +0800

    ✨ feat(添加自启动插件): 添加自启动插件

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,7 @@ pub mod plugins;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let mut builder = tauri::Builder::default()
+        .plugin(tauri_plugin_autostart::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_single_instance::init(|_, _, _| {}));
 
